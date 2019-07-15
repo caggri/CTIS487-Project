e2e419175f6df1a3903493232d2c2d0ba489d66a
DvpDen <Gone4Fun>
2019-07-15 16:48:14 +0300

    DataModel created (temperature, light, carbon).

---
+++ app/src/main/java/com/cagribayram/denizhancetin/project/DataModel.java
@@ -0,0 +1,15 @@
+package com.cagribayram.denizhancetin.project;
+
+public class DataModel {
+    private int temperature;
+    private String light;
+    private int carbon;
+
+    public DataModel(int temperature, String light, int carbon) {
+        this.temperature = temperature;
+        this.light = light;
+        this.carbon = carbon;
+    }
+
+
+}
